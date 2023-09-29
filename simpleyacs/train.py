d6dec2eb55134d6a9069e757f1df7e56bed3ff86
Christopher Boyle <chrismjboyle@gmail.com>
2023-09-29 16:38:12 +0100

    Store losses in train.py
    and placeholder for storing metrics...

--- simpleyacs/train.py
+++ simpleyacs/train.py
@@ -1,4 +1,5 @@
 from tqdm import trange
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
@@ -35,6 +36,10 @@ class Trainer:
         loss_func = torch.nn.MSELoss()
         dl = DataLoader(self.ds, batch_size=self.batch_size)
 
+        epochs = []
+        losses = []
+        # metrics, e.g. accuracy
+        accuracies = []
         bar = trange(self.n_epochs, unit='epochs', desc='--')
         for epoch in bar:
             epoch_loss = 0.0
@@ -44,6 +49,7 @@ class Trainer:
                 loss.backward()
                 opt.step()
                 epoch_loss += loss.item()
+            epoch_loss /= len(dl)
             bar.set_description(f'loss={epoch_loss:.1f}')
 
             if epoch % 5 == 0:
@@ -51,4 +57,18 @@ class Trainer:
                 torch.save(self.model.state_dict(), f'{self.output_dir}/state_at_{epoch=}.pt')
 
             # after the training epoch, you'd want to do validation too.
+            # accuracy = ...
+
+            # store data, to be save later
+            epochs.append(epoch)
+            losses.append(epoch_loss)
+
+            # if validating...
+            # accuracies.append(accuracy)
+
+        # when training's complete, you probably want to save the training results
+        np.savetxt(f'{self.output_dir}/losses.csv', np.array([epochs, losses]).T)
+
+        # obviously, your run will have valid losses and metrics to store, too. Add valid loss above, and metrics below.
+        # np.savetxt(f'{self.output_dir}/accuracy.csv', np.array([epochs, accuracy]).T)
 
